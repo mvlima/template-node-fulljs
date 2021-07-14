a55c2659b9b66ad6f16995092c5343b351b42bad
Marcus Vieira <vieira.mvlima@gmail.com>
2021-07-14 13:55:11 -0300

    Added scaling for the buttons on hover

--- src/views/pages/components/float/floatImage.js
+++ src/views/pages/components/float/floatImage.js
@@ -2,8 +2,9 @@ import BabyYoda from '../../../../assets/images/elements/baby-yoda.png'
 
 let floatImage = `
 <div class="main-left">
-<a href="#section-two">
-    <img src="${BabyYoda}" alt="Baby Yoda">
+    <a href="#section-two">
+        <img src="${BabyYoda}" alt="Baby Yoda">
+    </a>
 </div>
 `
 
