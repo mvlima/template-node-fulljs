db3b55a3846111fae88b68586498a76c48b00978
Marcus Vieira <vieira.mvlima@gmail.com>
2021-07-14 14:36:55 -0300

    added scaling images

--- src/views/pages/components/float/floatImage.js
+++ src/views/pages/components/float/floatImage.js
@@ -3,7 +3,7 @@ import BabyYoda from '../../../../assets/images/elements/baby-yoda.png'
 let floatImage = `
 <div class="main-left">
     <a href="#section-two">
-        <img src="${BabyYoda}" alt="Baby Yoda">
+        <img src="${BabyYoda}" class="scaling-image" alt="Baby Yoda">
     </a>
 </div>
 `
